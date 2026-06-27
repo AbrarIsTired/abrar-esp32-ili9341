99ba23bee1f728a30268d9852d3b6f1424abf666
Abrar <167377939+AbrarIsTired@users.noreply.github.com>
2026-06-27 19:12:43 -0400

    Loops testing/benchmark
    Loops testing/benchmark to test how many lines I can comfortably have on screen at once

---
+++ basic/loops_test.py
@@ -0,0 +1,24 @@
+# loops_test.py
+
+from machine import Pin, SPI
+from ili9341 import ILI9341, color565
+
+backlight = Pin(21, Pin.OUT)
+backlight.value(1)
+
+spi = SPI(1, baudrate=10000000, sck=Pin(14), mosi=Pin(13), miso=Pin(12))
+
+dummy_rst = Pin(4, Pin.OUT)
+dummy_rst.value(1)
+
+display = ILI9341(spi, cs=Pin(15), dc=Pin(2), rst=dummy_rst, w=320, h=240, r=0)
+
+display.set_color(color565(0, 0, 0), color565(255, 255, 255))
+display.erase()
+display.reset_scroll()
+
+display.set_pos(2, 5)
+for i in range(38):
+    display.print("Line number {} of the test".format(i))
+
+print("Done - check the screen")
